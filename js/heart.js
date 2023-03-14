fd343a892ed65841df39b791647a4fca0e128e6f
yeyeah7 <1324858193@qq.com>
2023-03-14 21:39:43 +0800

    Site updated: 2023-03-14 21:39:43

---
+++ js/heart.js
@@ -0,0 +1,9 @@
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2023 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Yeyeahl');
+})
+
+// 移除页脚样式
+var app = document.getElementById('footer');
+app.removeAttribute('id')
+
